4e95df55843f10cbabdce67c20bb9f02f5cf5e83
Tatsuya Domoto <tatsuyad29@gmail.com>
2018-03-11 01:12:10 +0900

    devise以外の整備

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,19 +1,44 @@
 class SubscriptionsController < ApplicationController
   def index
+    # @subscriptions = Subscription.includes(:user).order("created_at ASC")
   end
 
   def new
   end
 
-  def edit
-  end
-
-  def show
-  end
-
   def create
+    # Subscription.create(title: subscription_params[:title], sum: subscription_params[:sum], user_id: current_user.id)
   end
 
   def destroy
+    # subscription = Subscription.find(params[:id])
+    # subscription.destroy if subscription.user.id == current_user.id
   end
+
+  def edit
+     # @subscriptions = Subscription.find(params[:id])
+  end
+
+  def update
+    # subscription = Subscription.find(params[:id])
+    # if subscription.user.id == current_user.id
+    #   subscription.update(subscription_params)
+    # end
+  end
+
+  def show
+    # @subscription = Subscription.find(params[:id])
+    # @memo = @subscription.memo.includes(:user)
+  end
+
+  # private
+  # def subscription_params
+  #   params.permit()
+  # end
+
+  # def move_to_top
+  #   unless user_signed_in?
+  #     redirect_to "/"
+  #   end
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310122748) do
+ActiveRecord::Schema.define(version: 20180310155613) do
+
+  create_table "memos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subscriptions", force: :cascade do |t|
     t.datetime "created_at", null: false
